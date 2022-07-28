302052b466c99f21e3ed8248655cac9229455795
Santiago Benegas <santiagobenegas03@gmail.com>
2022-07-28 17:38:11 -0300

    feat: responsive menu

---
+++ main.js
@@ -0,0 +1,11 @@
+const navBar = document.querySelector(".nav");
+const menu = document.querySelector(".fa-bars");
+
+
+
+function showMenu() {
+    navBar.style.overflow == "visible" ? navBar.style.overflow = "hidden" : navBar.style.overflow = "visible";
+}
+
+
+
