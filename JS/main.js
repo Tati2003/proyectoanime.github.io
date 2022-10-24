ce513f8dcf8fd3b9f6800c7601dbbaa4a8e1d4ea
Santiago Benegas <santiagobenegas03@gmail.com>
2022-10-24 15:56:52 -0300

    feat: menu and js changed

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,43 @@
+const navBar = document.querySelector(".nav");
+const menu = document.querySelector(".fa-bars");
+const upButton = document.getElementById("upBtn");
+
+
+/*When a client refresh the page it goes back to the beginning */
+
+window.onload = () => {
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'smooth'
+    })
+};
+
+
+
+/* When a client stay at certain point, the up Button will be available */
+
+window.onscroll = function () { showUpButton() };
+
+function showUpButton() {
+    return (document.body.scrollTop > 850 || document.documentElement.scrollTop > 850) ? upButton.style.display = "block" : upButton.style.display = "none";
+}
+
+/*The upButton function */
+
+function goTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+
+}
+
+/* It shows the menu if there is a mobile client */
+function showMenu() {
+    navBar.style.overflow == "visible" ? navBar.style.overflow = "hidden" : navBar.style.overflow = "visible";
+}
+
+
+
+
+
 
